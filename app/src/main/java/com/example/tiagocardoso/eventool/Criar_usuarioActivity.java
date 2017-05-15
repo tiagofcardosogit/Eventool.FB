c4373fa336c02a8eada2fd28857a8214d195294c
Tiago Fabricio Cardoso <tiago.cardoso@opet.edu.br>
2017-05-15 14:02:08 -0300

    logando via face e navegando entre as telas iniciais

--- app/src/main/java/com/example/tiagocardoso/eventool/Criar_usuarioActivity.java
+++ app/src/main/java/com/example/tiagocardoso/eventool/Criar_usuarioActivity.java
@@ -1,8 +1,13 @@
 package com.example.tiagocardoso.eventool;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Criar_usuarioActivity extends AppCompatActivity {
@@ -15,6 +20,19 @@ public class Criar_usuarioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_criar_usuario);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        //Cadastando usuario
+        firebaseAuth.createUserWithEmailAndPassword("tiago.cardoso@opet.edu.br","tiago123") // preicisa alterar para recuperar o email e senha da tela
+                .addOnCompleteListener(Criar_usuarioActivity.this, new OnCompleteListener<AuthResult>() {  //interface
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) { //testando se o cadastro foi com sucesso
+                        if ( task.isSuccessful()) {
+                            Log.i("createUser", "Sucesso ao cadastrar");
+                        }else {
+                            Log.i("createUser", "Falha ao cadastrar");
+                        }
+                    }
+                });
 
 
     }
