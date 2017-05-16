0dce0b01efab4fb5a26b87a0269ac21884b57a5c
Tiago Fabricio Cardoso <tiago.cardoso@opet.edu.br>
2017-05-16 13:22:29 -0300

    Logando via Facebook, email e senha e criando usuarios

--- app/src/main/java/com/example/tiagocardoso/eventool/Activities/LoginActivity.java
+++ app/src/main/java/com/example/tiagocardoso/eventool/Activities/LoginActivity.java
@@ -16,6 +16,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 /**
  * A login screen that offers login via email/password.
@@ -35,11 +38,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        userIsLogged();
+
 
         email = (EditText) findViewById(R.id.emaiLoginID);
         senha = (EditText) findViewById(R.id.senhaLoginID);
         btnLogin = (Button) findViewById(R.id.btnLoginID);
 
+
+        //momento em que é capturado email e senha ao clicar no botao 'BORA'
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,6 +59,7 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    //valida o login se as credenciais estao corretas
     public void validarLogin(){
 
         autenticacao = ConfigFirebase.getFirebaseAutenticacao();
@@ -66,10 +74,32 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Login com Sucesso", Toast.LENGTH_SHORT).show();
                     abrirTelaPrincipal();
                 }else{
-                    Toast.makeText(LoginActivity.this, "Falha no Login", Toast.LENGTH_SHORT).show();
+
+                    String erroExcecao = "";
+
+                    try{
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) { //metodo para lancar excecao quando o usuario esta errado
+                        erroExcecao = "Falha ao Realizar o Login";
+
+                    }catch (FirebaseAuthInvalidCredentialsException e){ //metodo para lancar excecao quando e email é invalido
+                        erroExcecao = "Email inválido, digite novamente!";
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this, "Erro: " + erroExcecao, Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    private void userIsLogged(){ // Vefica se o usuario já esta logado
+        autenticacao = ConfigFirebase.getFirebaseAutenticacao();
+
+        if (autenticacao.getCurrentUser() != null){
+            abrirTelaPrincipal();
+        }
     }
 
     private void abrirTelaPrincipal(){
