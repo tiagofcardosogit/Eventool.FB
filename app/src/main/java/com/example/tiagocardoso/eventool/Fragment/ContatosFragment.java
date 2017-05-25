40c6c5bccedcb2b8763eb7e8590dbc29da34a965
Tiago Fabricio Cardoso <tiago.cardoso@opet.edu.br>
2017-05-25 18:04:34 -0300

    Logando via Facebook, email e senha e criando usuarios e eventos e tbm com Maps

--- app/src/main/java/com/example/tiagocardoso/eventool/Fragment/ContatosFragment.java
+++ app/src/main/java/com/example/tiagocardoso/eventool/Fragment/ContatosFragment.java
@@ -6,14 +6,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.tiagocardoso.eventool.Config.ConfigFirebase;
+import com.example.tiagocardoso.eventool.Helper.Preferencias;
 import com.example.tiagocardoso.eventool.R;
+import com.example.tiagocardoso.eventool.model.Contato;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
 
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> contatos;
+    private DatabaseReference firebase;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -23,8 +39,57 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Array de contatos
+        contatos = new ArrayList<>();
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        listView = (ListView) view.findViewById(R.id.lv_contatos);
+        adapter = new ArrayAdapter(
+                getActivity(),
+                R.layout.listview_customizado,
+                contatos
+        );
+        listView.setAdapter(adapter);
+
+        //listando contatos do Firebase
+        Preferencias preferencias = new Preferencias(getActivity());
+        String identificadorUsuarioLogado = preferencias.getIdentificador();
+
+        firebase = ConfigFirebase.getFirebase()
+                .child("contatos")
+                .child(identificadorUsuarioLogado);
+
+        firebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //limpa array de contatos
+                contatos.clear();
+
+                //listar os dados percorrento todos os contatos
+                for (DataSnapshot dados : dataSnapshot.getChildren()){
+                    Contato contato = dados.getValue(Contato.class);
+                    contatos.add(contato.getNome());
+                    //contatos.add(contato.getEmail());
+
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        return view;
     }
 
 }
